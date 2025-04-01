ea4576529e222b78b27c58818b581bcc756ac7cc
AntoineChapel <antoine.chapel@sciencespo.fr>
2025-03-31 21:24:14 -0400

    update main

--- main.jl
+++ main.jl
@@ -14,6 +14,20 @@ df_agg = combine(
     groupby(df, [:w_geocode_11, :h_geocode_11]),
     Not([:w_geocode_11, :h_geocode_11]) .=> sum
 )
+#w_geocode: workplace census tract code
+#h_geocode: home census tract code
+#S000: total number of jobs
+#SA01: number of workers age 29 or younger
+#SA02: number of workers age 30-54
+#SA03: number of workers age 55 or older
+#SE01: number of jobs with earnings 1250 or less
+#SE02: number of jobs with earnings 1251-3333
+#SE03: number of jobs with earnings above 3334
+#SI01: number of jobs in goods producing industry sectors
+#SI02: number of jobs in Trade, Transportation and Utilities
+#SI03: number of jobs in All Other Services
 
 
 println(first(df_agg, 15))
+println(describe(df_agg[:, Not([:w_geocode_11, :h_geocode_11])]))
+println("Number of rows: ", size(df_agg, 1))
